da9b6c0a07faf97ca7c55dd71fa61ebeddb0307d
Le Nguyen <nle20904@gmail.com>
2026-02-02 17:47:49 +0700

    Add files via upload

--- Data_Cleaning_Example_Query.sql
+++ Data_Cleaning_Example_Query.sql
@@ -1,54 +1,112 @@
 ﻿/* ============================================================
    DoonieWatch - Data Cleaning Example (Jan-2026)
    Author: Nguyen Le
-   Description: In my previous project, I performed data cleaning using Excel. 
-   In this project, I will focus on cleaning and processing the data using SQL.
-	For demonstration purposes, I will only use the sales data from January 2026. 
-	The main objective of this project is to showcase my data cleaning techniques and workflow in SQL.
+
+   Description:
+   In my previous project, I performed data cleaning using Excel.
+   In this project, I focus on cleaning and processing data using SQL.
+
+   For demonstration purposes, I only use sales data from January 2026.
+   The main objective of this project is to showcase my data cleaning
+   techniques and workflow in SQL.
 ============================================================ */
+
+---------------------------------------------------------------
+-- View raw data
+---------------------------------------------------------------
 SELECT *
-FROM DoonieWatch.dbo.ProductPF
-
--- Xóa những sản phẩm không hoạt động và những sản phẩm không có phân loại và những phần không có thông tin
-Delete from ProductPF 
-where [Tên Phân Loại] = '-' 
-or [Tình trạng sản phẩm hiện tại] = N'Đã xóa'
-
--- Breaking out 'Tên phân loại' into Individual Columns ( 'Tên phân loại', 'Size')
-Select 
-[Sản phẩm],
-Substring([Tên Phân Loại],1,Charindex(',',[Tên Phân Loại]+',') -1) as [Mẫu],
-Substring([Tên Phân Loại],Charindex(',',[Tên Phân Loại]) +1,Len([Tên Phân Loại])) as [Size]
-FROM DoonieWatch.dbo.ProductPF
-
-Alter Table ProductPF
-Add [Mẫu] nvarchar(255),[Size] nvarchar(255);
-
-Update ProductPF
-Set 
-	[Mẫu] = Substring([Tên Phân Loại],1,Charindex(',',[Tên Phân Loại]+',') -1),
-	[Size] = Substring([Tên Phân Loại],Charindex(',',[Tên Phân Loại]) +1,Len([Tên Phân Loại]));
-
-Select 
-[Sản phẩm],[Mẫu],[Size]
-FROM DoonieWatch.dbo.ProductPF
-
--- Rút gọn tên sản phẩm theo thương hiệu
-Select Distinct([Sản phẩm]),count([Sản phẩm])
-FROM DoonieWatch.dbo.ProductPF
-Group by [Sản phẩm]
-
-Update ProductPF
-Set 
-	[Sản phẩm]= CASE
-		WHEN [Sản phẩm] LIKE N'%MVD%' THEN 'Movado'
-		WHEN [Sản phẩm] LIKE N'%A171%' THEN N'Casio tròn'
-		WHEN [Sản phẩm] LIKE N'%DW%' THEN 'DW'
-		WHEN [Sản phẩm] LIKE N'%WRO6%' THEN 'Casio'
-		WHEN [Sản phẩm] LIKE N'%Pindows%' THEN 'PINDOWS'
-		WHEN [Sản phẩm] LIKE N'%RLEX%' THEN 'Rolex'
-		ELSE [Sản phẩm]
-		END
 FROM DoonieWatch.dbo.ProductPF;
 
 
+---------------------------------------------------------------
+-- Remove inactive products and invalid categories
+---------------------------------------------------------------
+DELETE
+FROM DoonieWatch.dbo.ProductPF
+WHERE [Tên Phân Loại] = '-'
+   OR [Tình trạng sản phẩm hiện tại] = N'Deleted';
+
+
+---------------------------------------------------------------
+-- Preview: Split "Category Name" into Model and Size
+---------------------------------------------------------------
+SELECT
+    [Sản phẩm],
+
+    SUBSTRING(
+        [Tên Phân Loại],
+        1,
+        CHARINDEX(',', [Tên Phân Loại] + ',') - 1
+    ) AS [Model],
+
+    SUBSTRING(
+        [Tên Phân Loại],
+        CHARINDEX(',', [Tên Phân Loại] + ',') + 1,
+        LEN([Tên Phân Loại])
+    ) AS [Size]
+
+FROM DoonieWatch.dbo.ProductPF;
+
+
+---------------------------------------------------------------
+-- Add new columns: Model and Size
+---------------------------------------------------------------
+ALTER TABLE ProductPF
+ADD
+    [Model] NVARCHAR(255),
+    [Size]  NVARCHAR(255);
+
+
+---------------------------------------------------------------
+-- Populate Model and Size columns
+---------------------------------------------------------------
+UPDATE ProductPF
+SET
+    [Model] = SUBSTRING(
+                  [Tên Phân Loại],
+                  1,
+                  CHARINDEX(',', [Tên Phân Loại] + ',') - 1
+              ),
+
+    [Size] = SUBSTRING(
+                 [Tên Phân Loại],
+                 CHARINDEX(',', [Tên Phân Loại] + ',') + 1,
+                 LEN([Tên Phân Loại])
+             );
+
+
+---------------------------------------------------------------
+-- Verify extracted columns
+---------------------------------------------------------------
+SELECT
+    [Sản phẩm],
+    [Model],
+    [Size]
+FROM DoonieWatch.dbo.ProductPF;
+
+
+---------------------------------------------------------------
+-- Review product names
+---------------------------------------------------------------
+SELECT
+    [Sản phẩm],
+    COUNT(*) AS Product_Count
+FROM DoonieWatch.dbo.ProductPF
+GROUP BY [Sản phẩm];
+
+
+---------------------------------------------------------------
+-- Standardize product names by brand
+---------------------------------------------------------------
+UPDATE ProductPF
+SET
+    [Sản phẩm] =
+        CASE
+            WHEN [Sản phẩm] LIKE N'%MVD%'     THEN 'Movado'
+            WHEN [Sản phẩm] LIKE N'%A171%'    THEN N'Casio Round'
+            WHEN [Sản phẩm] LIKE N'%DW%'      THEN 'DW'
+            WHEN [Sản phẩm] LIKE N'%WRO6%'    THEN 'Casio'
+            WHEN [Sản phẩm] LIKE N'%Pindows%' THEN 'Pindows'
+            WHEN [Sản phẩm] LIKE N'%RLEX%'    THEN 'Rolex'
+            ELSE [Sản phẩm]
+        END;
